091679867c52fe86ba62c8959b89dd4379f776d2
Nursel ALTIN <nurselaltin.na@gmail.com>
2020-07-06 09:06:37 +0300

    Admin:Front\Homepage.

---
+++ blog/app/Http/Controllers/Front/Homepage.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Models\Articles;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Page;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+
+class Homepage extends Controller
+{
+    function __construct()
+    {
+        view()->share('pages',Page::orderBy('created_at','ASC')->get());
+        view()->share('categories', Category::inRandomOrder()->get());
+    }
+
+    function  index(){
+
+       $data['categories']= Category::inRandomOrder()->get();
+       $data['articles']  = Articles::orderBy('created_at','DESC')->paginate(2);
+       $data['articles']->withPath(url('sayfa'));
+
+          return view('front.homepage',$data);
+    }
+
+    public  function singlePost($category,$slug){
+
+        //category var mı kontrol et?
+        $category = Category::whereSlug($category)->first() ?? abort(403,'Böyle bir kategori bulunamadı.');
+
+        //slug var mı kontrol et?,Kategoriye ait yazı var mı kontrol et
+        $article = Articles::where('slug',$slug)->whereCategoryId($category->id)->first() ?? abort(403,'Böyle bir yazı bulunamadı.');
+        //where ile sütün adını direk birleştererekte yapabiliriz
+        //$data['article']  = Articles::whereSlug($slug)->first() ?? abort(403,'Sanırım kayboldun');
+        $article->increment('view');
+        $data['article'] = $article;
+        return view('front.singlePost',$data);
+    }
+
+    public  function categoriesPost($category_slug){
+
+        $category = Category::whereSlug($category_slug)->first() ?? abort(403,'Böyle bir kategori bulunamadı.');
+        $data['articles'] = Articles::whereCategoryId($category->id)->orderBy('created_at','DESC')->paginate(2) ;
+        $data['category'] = $category;
+
+        return view('front.categoriesPost',$data);
+    }
+
+    public  function  pages($slug){
+           $page = Page::whereSlug($slug)->first() ?? abort(403,'Böyle bir sayfa bulunamadı.');
+           $data['page'] = $page;
+        return view('front.pages',$data);
+    }
+
+    public  function  contact(){
+
+        return view('front.contact');
+    }
+    public  function  contactPost(Request  $request){
+
+        $contact = new Contact;
+
+        $contact->fullname = $request->fullname;
+        $contact->email = $request->email;
+        $contact->message = $request->message;
+        $contact->konu = $request->konu;
+        $contact->save();
+        return redirect()->route('contact')->with('success','Mesajınız bize iletildi.Teşekkür ederiz.');
+
+
+    }
+}
